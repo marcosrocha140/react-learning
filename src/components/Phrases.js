882a83adc4049fbabc0aa8f7eff14e2d8f9db7fd
Marcos <rmarcos140@gmail.com>
2023-12-01 14:17:58 -0300

    add folder components

---
+++ src/components/Phrases.js
@@ -0,0 +1,10 @@
+function Phrases(){
+    return(
+        <div>
+            <h1>My first Component</h1>
+            <p>this is my first Component in react</p>
+        </div>
+    )
+}
+
+export default Phrases
