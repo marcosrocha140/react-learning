54039ada5673097517ecdbcea72dfe84e418f95c
Marcos <rmarcos140@gmail.com>
2023-12-01 15:18:52 -0300

    create components files

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+function Nav(){
+    return(
+        <div>
+            <nav>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
--- src/components/Phrases.js
+++ src/components/Phrases.js
@@ -2,7 +2,7 @@ function Phrases(){
     return(
         <div>
             <h1>My first Component</h1>
-            <p>this is my first Component in react</p>
+            
         </div>
     )
 }
